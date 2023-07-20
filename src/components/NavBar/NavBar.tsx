b030b963e8ebf520166d0ee9bc8a4bbafa905fe0
guillerminaDiaz <guillerminaadiaz@gmail.com>
2023-07-20 14:23:32 -0300

    navbar logo and css

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,11 @@
 import { NavLink } from "react-router-dom";
+import style from "./NavBar.module.css";
+import logo from "../../images/GD.png";
+
 const NavBar = () => {
   return (
-    <div>
+    <div className={style.container}>
+      <img src={logo} alt="personal logo" className={style.img} />
       <NavLink to="/">Intro</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/skills">Skills</NavLink>
