917ecbc8983372a09a33bed93291fc4df427db0f
guillerminaDiaz <guillerminaadiaz@gmail.com>
2023-07-21 21:24:34 -0300

    home background

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,12 +2,13 @@ import About from "../About/About";
 import Contact from "../Contact/Contact";
 import Projects from "../Projects/Projects";
 import Skills from "../Skills/Skills";
+import style from "./Home.module.css";
 
 const Home = () => {
   return (
     <div>
-      <div style={{ height: "100%", width: "100%" }}>
-        <h3>Hi I´m Guillermina</h3>
+      <div className={style.container}>
+        <h3 style={{ marginTop: 0 }}>Hi I´m Guillermina</h3>
         <h1>Full Stack Developer</h1>
         <a
           href="https://docs.google.com/document/d/1JAcvrJwgtZvf6ujYA_1T7qD6mKM_j9GUuUjL7aMO4F8/edit?usp=sharing"
